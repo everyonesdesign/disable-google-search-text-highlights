acc44800fc7f5d3390fc373831f1f9f2441de6ce
Yura Trambitckii <tr.yura@yahoo.com>
2020-08-25 09:09:09 +0200

    Handle the case with links wihout "href" attribute

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,7 +1,9 @@
 try {
   if (/google\.\w+$/.test(location.hostname)) {
     [...document.querySelectorAll('a')].forEach((link) => {
-      link.href = link.href.replace(/#:~:.*$/, '');
+      if (link.href) {
+        link.href = link.href.replace(/#:~:.*$/, '');
+      }
     });
   }
 } catch(e) {}
