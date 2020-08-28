2987a741523bc2a48cdd0cbf38c50aea76570192
Yura Trambitckii <tr.yura@yahoo.com>
2020-08-28 16:23:37 +0200

    Fix an issue with replacing undefined/null hrefs

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,8 +1,9 @@
 try {
   if (/google\.\w+$/.test(location.hostname)) {
     [...document.querySelectorAll('a')].forEach((link) => {
-      if (link.href) {
-        link.href = link.href.replace(/#:~:.*$/, '');
+      const regex = /#:~:.*$/;
+      if (typeof link.href === 'string' && regex.test(link.href)) {
+        link.href = link.href.replace(regex, '');
       }
     });
   }
