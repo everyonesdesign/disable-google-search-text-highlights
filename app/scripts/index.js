aeacddc4e389d7a8d870ea564dc3fef0025c4df5
Yura Trambitckii <tr.yura@yahoo.com>
2020-09-05 12:32:33 +0200

    Improve regex for Google domain detection

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,7 @@
 try {
-  if (/google\.\w+$/.test(location.hostname)) {
+  // See https://ipfs.io/ipfs/QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco/wiki/List_of_Google_domains.html
+  // for the full list of Google domains
+  if (/google\.((com?\.)?\w+|)$/.test(location.hostname)) {
     [...document.querySelectorAll('a')].forEach((link) => {
       const regex = /#:~:.*$/;
       if (typeof link.href === 'string' && regex.test(link.href)) {
