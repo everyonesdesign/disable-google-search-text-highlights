a43e17e976ceb2f9f372ae1451049bd404df760e
Yura Trambitckii <tr.yura@yahoo.com>
2020-10-08 20:47:28 +0200

    Only replace "text" fragment directive
    This extension is aimed at Google search highlights removal
    
    The highlights are implemented via "text" fragment directive
    
    However there might be other directive, separated with "&"
    (see [the standard](https://wicg.github.io/scroll-to-text-fragment/#parsing-the-fragment-directive))
    
    This change removes the whole fragment directive section
    if "text" is the only directive or removes "text" keeps the rest if there're some others
    
    Bonus feature: closes #3

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,8 +1,21 @@
 try {
+  const TEXT_HIGHLIGHT_DIRECTIVE_ID = 'text';
+  const FRAGMENT_DIRECTIVE_SEPARATOR = ':~:';
+
   [...document.querySelectorAll('a')].forEach((link) => {
-    const regex = /#:~:.*$/;
-    if (typeof link.href === 'string' && regex.test(link.href)) {
-      link.href = link.href.replace(regex, '');
+    if (typeof link.href === 'string' && link.href.includes(FRAGMENT_DIRECTIVE_SEPARATOR)) {
+      const url = new URL(link.href);
+      const [ hash, fragmentDirectivesString ] = url.hash.split(FRAGMENT_DIRECTIVE_SEPARATOR);
+
+      const fragmentDirectives = new URLSearchParams(fragmentDirectivesString);
+      fragmentDirectives.delete(TEXT_HIGHLIGHT_DIRECTIVE_ID);
+      const newFragmentDirectivesString = fragmentDirectives.toString();
+
+      url.hash = newFragmentDirectivesString ?
+        `${hash}${FRAGMENT_DIRECTIVE_SEPARATOR}${newFragmentDirectivesString}` :
+        hash;
+
+      link.href = url.href;
     }
   });
 } catch(e) {}
