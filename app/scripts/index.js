bc1424a0a19edc1286934a27f0eace28b23ab4a2
Yura Trambitckii <tr.yura@yahoo.com>
2022-08-10 13:35:46 +0200

    feat: use runPatiently function to run as early as possible

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -78,6 +78,76 @@
     return;
   }
 
-  resetLinks();
-  setInterval(resetLinks, 100);
+    /**
+   * Try to run a snippet multiple times, until it succeeds
+   * (success it judged by the value returned from the callback)
+   *
+   * example of usage:
+   *  runPatiently({
+   *    fn: () => {
+   *      const elem = document.querySelector('<...>');
+   *      if (elem) {
+   *        elem.style.display = 'none';
+   *        return true;
+   *      }
+   *   }
+   * });
+   * @param {Object} params - configuration of the run
+   * @param {Function} params.fn - function to run (should return true when run successfully)
+   * @param {Number?} params.intervalMs - ms between attempts
+   * @param {Number?} params.giveupMs - ms before give up
+   * @param {Number?} params.startDelayMs - wait a bit before starting attempting runs
+   * @param {Function?} params.onSuccess - callback on success
+   * @param {Function?} params.onGiveup - callback on giving up
+   * @param {Function?} params.onComplete - callback on finish in any case
+   */
+  function runPatiently(params) {
+    const {
+      fn,
+      intervalMs = 100,
+      giveupMs = 10000,
+      startDelayMs,
+      onSuccess = () => {},
+      onGiveup = () => {},
+      onComplete = () => {},
+    } = params;
+
+    let giveUpTimeout;
+
+    if (intervalMs > giveupMs) throw new Error('giveupMs should be bigger than intervalMs');
+
+    if (typeof startDelayMs !== 'undefined') {
+      setTimeout(() => runPatiently({ ...params, startDelayMs: undefined }), startDelayMs);
+      return;
+    }
+
+    const interval = setInterval(() => {
+      const result = fn();
+      if (result) {
+        clearInterval(interval)
+        clearTimeout(giveUpTimeout);
+        setTimeout(() => {
+          onSuccess();
+          onComplete();
+        });
+      };
+    }, intervalMs);
+
+    giveUpTimeout = setTimeout(() => {
+      clearInterval(interval);
+      setTimeout(() => {
+        onGiveup();
+        onComplete();
+      });
+    }, giveupMs);
+  }
+
+  runPatiently({
+    fn: () => {
+      if (document.querySelector(`a[href*="#${FRAGMENT_DIRECTIVE_SEPARATOR}"]`)) {
+        resetLinks();
+        return true;
+      }
+    },
+  });
 }());
