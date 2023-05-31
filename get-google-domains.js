a3159067e47f16ff01842e3e760705b37211a182
Yura Trambitckii <tr.yura@yahoo.com>
2023-05-31 20:32:35 +0200

    feat: retrieve the deprecated domains
    Recalling the domains might trigger
    prompt from the users already using the extension
    
    There's no need to ask for that the 2nd time

--- get-google-domains.js
+++ get-google-domains.js
@@ -1,11 +1,24 @@
 const fetch = require('node-fetch');
 
+/*
+ * Google removed these domains from the supported list
+ * They're still kept here for 2 reasons:
+ * - at some point Google might want to bring them back
+ * - removing domains from manifest might trigger a redundant prompt from users to re-confirm permissions?
+*/
+const LEGACY_DOMAINS = [
+  '.google.vg',
+  '.google.ms',
+  '.google.com.ai',
+];
+
 fetch('https://www.google.com/supported_domains')
     .then(res => res.text())
     .then(body => {
       const domains = body
         .trim()
         .split(/\s/)
+        .concat(LEGACY_DOMAINS)
         .sort()
         .map(domain => `        "https://www${domain}/search?*"`)
         .join(',\n');
